7fa880c57b55934aef750e1ed6e8770cd35bcfc3
mbenmans <mbenmans@redhat.com>
2017-07-27 12:30:16 +0200

    adding file writing

--- routes/alerts.js
+++ routes/alerts.js
@@ -2,17 +2,17 @@ var express = require('express');
 var router = express.Router();
 var thenWriteJson = require('then-write-json');
 
-var alertsDir = process.env.ALERTS_DIR || '/alerts/'
+var alertsDir = process.env.ALERTS_DIR ||  '/alerts/'
 
 router.post('/', function (req, res, next) {
     var fileName = alertsDir + Date.now() + '.json'
     thenWriteJson(fileName, req.body)
         .then(function (res) {
             console.log(fileName + ' written');
-            //=> true 
         })
         .catch(console.error)
-    res.send(JSON.stringify(req.body, null, 2));
+    res.sendStatus(200);
+
 
 });
 
